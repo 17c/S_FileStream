844cda9f60636c71c300ceba34f77d9b0cd89f28
hasee <hasee@WEEDS>
2018-04-29 15:34:13 +0800

    文件流

--- S_FileStream/Program.cs
+++ S_FileStream/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
+/// <summary>
+/// 学习文件流使用的Demo
+/// </summary>
 namespace S_FileStream
 {
     class Program
     {
+        public static FileInfo file;
+        public string filePath;
+
+        public FileInfo CreateFile(string path)
+        {
+            return new FileInfo(path);
+        }
+
         static void Main(string[] args)
         {
+            string file = Environment.CurrentDirectory;
+            Console.WriteLine("HelloWord!" + file);
+            foreach (var str in args)
+                Console.Write(str);
+            Console.ReadKey();
         }
     }
 }
+
